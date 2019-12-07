442254778cdfcd3f8a4eb8de176ac691a59c2840
heerzhang <herzhang@163.com>
2019-12-08 05:50:29 +0800

    准备改breakpoints

--- src/Theme/breakpoints.ts
+++ src/Theme/breakpoints.ts
@@ -14,3 +14,19 @@ export const generateMediaQueries = (points: BreakPointType) => ({
   xl: `@media (min-width: ${points.xl})`,
   hover: "@media (hover: hover)"
 });
+
+
+/* 旧的是， 参照修改。
+const breakpoints = {
+  sm: "567px",
+  md: "768px",
+  lg: "992px",
+  xl: "1200px"
+};const generateMediaQueries = (points: BreakPointType) => ({
+  sm: `@media (min-width: ${points.sm})`,
+  md: `@media (min-width: ${points.md})`,
+  lg: `@media (min-width: ${points.lg})`,
+  xl: `@media (min-width: ${points.xl})`,
+  hover: "@media (hover: hover)"
+});
+*/
